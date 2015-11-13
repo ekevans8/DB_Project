ab993d57e993af63b8d06d0889fda5dab1411953
polaris- <nagatospam@gmail.com>
2015-11-13 10:54:15 -0500

    Forgot comments

--- generate_password.php
+++ generate_password.php
@@ -1,10 +1,13 @@
 <?php
 
 function get_password_hash($password) {
+    // Generate a bcrypt encrypted password hash.
+    // This hash will always be 60 characters long.
     return password_hash($password, PASSWORD_BCRYPT);
 }
 
 function verify_password_hash($password, $hash) {
+    // Verify a bcrypt hashed password.
     return password_verify($password, $hash);
 }
 
