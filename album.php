6da60b49df7ad7747589660ffde18720264f30dc
polaris- <nagatospam@gmail.com>
2015-11-13 18:07:19 -0500

    Add/edit/remove songs

--- album.php
+++ album.php
@@ -16,6 +16,18 @@ function remove_album($albumId) {
     return true;
 }
 
+function add_song($title, $duration, $track_number) {
+    return true;
+}
+    
+function update_song($songId, $title, $duration, $track_number) {
+    return true;
+}
+
+function remove_song($songId) {
+    return true;
+}
+
 
 if(!isset($_GET['action'])) {
     die("An action must be specified");
@@ -157,42 +169,51 @@ else if($_GET['action'] == "addsong" || $_GET['action'] == "editsong") {
     $duration = "";
     $duration_error = "";
 
-    $name = "";
-    $name_error = "";
+    $track_number = "";
+    $track_number_error = "";
 
     if($_GET['action'] == "editsong" && isset($_GET['songId'])) {
-        $memberId = intval($_GET['songId']);    
+        $songId = intval($_GET['songId']);    
         $details = get_song_details($songId, $song_info);
         
         $title = $details['title'];
         $duration = $details['duration'];
-        $name = $details['name'];
+        $track_number = $details['track_number'];
     }
 
     if($_SERVER['REQUEST_METHOD'] === 'POST') {
-        $joinDate = sanitize_input($_POST['joinDate']);
-        $leaveDate = sanitize_input($_POST['leaveDate']);
-        $name = sanitize_input($_POST['name']);
+        $title = sanitize_input($_POST['title']);
+        $duration = sanitize_input($_POST['duration']);
+        $track_number = sanitize_input($_POST['track_number']);
         
-        $artistId = intval($_POST['artistId']);
+        $albumId = intval($_POST['albumId']);
             
-        if(isset($_POST['memberId'])) {
-            $memberId = intval($_POST['memberId']);
+        if(isset($_POST['songId'])) {
+            $songId = intval($_POST['songId']);
         }
         
         $has_error = false;
+        if(empty($title)) {
+            $title_error = "Title cannot be empty";
+            $has_error = true;
+        }
+        
+        if(empty($duration)) {
+            $duration_error = "Duration cannot be empty";
+            $has_error = true;
+        }
         
         if(!$has_error) {
             // Successful
             
-            if($memberId == -1) {
-                $ret = add_member($joinDate, $leaveDate, $name);
+            if($songId == -1) {
+                $ret = add_song($title, $duration, $track_number);
             } else {
-                $ret = update_member($memberId, $joinDate, $leaveDate, $name);
+                $ret = update_song($songId, $title, $duration, $track_number);
             }
             
             if(!$has_error) {
-                header('Location: artists.php?action=details&id=' . $artistId, true);
+                header('Location: album.php?action=details&id=' . $albumId, true);
                 die();
             }
         }
@@ -202,33 +223,33 @@ else if($_GET['action'] == "addsong" || $_GET['action'] == "editsong") {
     <form action="" method="POST">
     <table>
         <tr>
-            <td>Name:</td>
-            <td><input type="text" name="name" style="width:100%" value="<?=$name?>"></input>
-            <?php if(!empty($name_error)) { ?>
-            <span class="error">* <?=$name_error?></span>
+            <td>Title:</td>
+            <td><input type="text" name="title" style="width:100%" value="<?=$title?>"></input>
+            <?php if(!empty($title_error)) { ?>
+            <span class="error">* <?=$title_error?></span>
             <?php } ?>
             </td>
         </tr>
         <tr>
-            <td>Join Date:</td>
-            <td><input type="date" name="joinDate" style="width:100%" value="<?=$joinDate?>"></input>
-            <?php if(!empty($joinDate_error)) { ?>
-            <span class="error">* <?=$joinDate_error?></span>
+            <td>Duration:</td>
+            <td><input type="text" name="duration" style="width:100%" value="<?=$duration?>"></input>
+            <?php if(!empty($duration_error)) { ?>
+            <span class="error">* <?=$duration_error?></span>
             <?php } ?>
             </td>
         </tr>
         <tr>
-            <td>Leave Date:</td>
-            <td><input type="date" name="leaveDate" style="width:100%" value="<?=$leaveDate?>"></input>
-            <?php if(!empty($leaveDate_error)) { ?>
-            <span class="error">* <?=$leaveDate_error?></span>
+            <td>Track Number:</td>
+            <td><input type="number" name="track_number" min="0" style="width:100%" value="<?=$track_number?>"></input>
+            <?php if(!empty($track_number_error)) { ?>
+            <span class="error">* <?=$track_number_error?></span>
             <?php } ?>
             </td>
         </tr>
         <tr>
             <td colspan="2" align="center">
-                <input type="hidden" name="artistId" value="<?=$artistId?>">
-                <input type="hidden" name="memberId" value="<?=$memberId?>">
+                <input type="hidden" name="albumId" value="<?=$albumId?>">
+                <input type="hidden" name="songId" value="<?=$songId?>">
                 <input type="submit" value="Submit" style="width:100%"></input>
              </td>
         </tr>
@@ -244,8 +265,8 @@ else if($_GET['action'] == "deletemember") {
         die("Must specify id for this action");
     }
 
-    $memberId = intval($_GET['id']);
-    $ret = remove_member($memberId);
+    $songId = intval($_GET['id']);
+    $ret = remove_song($songId);
 
     // Check error code on delete?
     header('Location: artists.php?action=list', true);
