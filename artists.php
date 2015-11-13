83f00ecbba5ba0daf762a529def0b31ca7a20cb2
polaris- <nagatospam@gmail.com>
2015-11-13 14:57:58 -0500

    Added performance-related admin functions

--- artists.php
+++ artists.php
@@ -38,6 +38,22 @@ function is_artist_favorite($user_id, $artist_id) {
     return false;
 }
 
+function add_performance($venueId, $duration, $date) {
+    return true;
+}
+
+function update_performance($performanceId, $venueId, $duration, $date) {
+    return true;
+}
+
+function remove_performance($performanceId) {
+    return true;
+}
+
+function get_performance_details($performance_id) {
+    return null;
+}
+
 
 
 function make_release($albumid, $title, $recordlabel, $releasedate) {
@@ -152,138 +168,252 @@ foreach($artists_releases[$details['artistId']] as $release) {
 ?>
 <br>
 Performances:<br>
+<?php if(is_moderator()) { ?>
+<a href="artists.php?action=addperformance&id=<?=$details['artistId']?>">Add performance</a><br>
+<?php } ?>
 <br>
 Comments:<br>
 <?php
     }
 }
 else if($_GET['action'] == "addartist" || $_GET['action'] == "editartist") {
-is_moderator_or_die();
+    is_moderator_or_die();
 
-$name = "";
-$name_error = "";
+    $name = "";
+    $name_error = "";
 
-$formDate = "";
-$formDate_error = "";
+    $formDate = "";
+    $formDate_error = "";
 
-$breakupDate = "";
-$breakupDate_error = "";
+    $breakupDate = "";
+    $breakupDate_error = "";
 
-$formationZipcode = 0;
-$formationZipcode_error = "";
+    $formationZipcode = 0;
+    $formationZipcode_error = "";
 
-$artistId = -1;
+    $artistId = -1;
 
-if($_GET['action'] == "editartist" && isset($_GET['id'])) {
-    if(!isset($_GET['id'])) {
-        die("Must specify id for this action");
-    }
-    
-    $artistId = intval($_GET['id']);
-    
-    // Get artist information from database and set fields above
-    $details = get_artist_details($artistId, $artists);
-    
-    $name = $details['name'];
-    $formDate = $details['formDate'];
-    $breakupDate = $details['breakupDate'];
-    $formationZipcode = $details['formationZipcode'];
-}
-
-if($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $name = sanitize_input($_POST['name']);
-    $formDate = sanitize_input($_POST['formDate']);
-    $breakupDate = sanitize_input($_POST['breakupDate']);
-    $formationZipcode = sanitize_input($_POST['formationZipcode']);
-    
-    if(isset($_POST['artistId'])) {
-        $artistId = intval($_POST['artistId']);
-    }
-    
-    $has_error = false;
-    if(empty($name)) {
-        $name_error = "Artist name cannot be empty";
-        $has_error = true;
-    }
-    
-    if($formationZipcode <= 9999) {
-        $formationZipcode_error = "Formation zipcode must be 5 digits";
-        $has_error = true;
-    }
-    
-    if(!$has_error) {
-        // Successful
+    if($_GET['action'] == "editartist" && isset($_GET['id'])) {
+        if(!isset($_GET['id'])) {
+            die("Must specify id for this action");
+        }
         
-        if($artistId == -1) {
-            $ret = add_artist($name, $formDate, $breakupDate, $formationZipcode);
-        } else {
-            $ret = update_artist($artistId, $name, $formDate, $breakupDate, $formationZipcode);
+        $artistId = intval($_GET['id']);
+        
+        // Get artist information from database and set fields above
+        $details = get_artist_details($artistId, $artists);
+        
+        $name = $details['name'];
+        $formDate = $details['formDate'];
+        $breakupDate = $details['breakupDate'];
+        $formationZipcode = $details['formationZipcode'];
+    }
+
+    if($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $name = sanitize_input($_POST['name']);
+        $formDate = sanitize_input($_POST['formDate']);
+        $breakupDate = sanitize_input($_POST['breakupDate']);
+        $formationZipcode = sanitize_input($_POST['formationZipcode']);
+        
+        if(isset($_POST['artistId'])) {
+            $artistId = intval($_POST['artistId']);
+        }
+        
+        $has_error = false;
+        if(empty($name)) {
+            $name_error = "Artist name cannot be empty";
+            $has_error = true;
+        }
+        
+        if($formationZipcode <= 9999) {
+            $formationZipcode_error = "Formation zipcode must be 5 digits";
+            $has_error = true;
         }
         
         if(!$has_error) {
-            header('Location: artists.php?action=list', true);
-            die();
+            // Successful
+            
+            if($artistId == -1) {
+                $ret = add_artist($name, $formDate, $breakupDate, $formationZipcode);
+            } else {
+                $ret = update_artist($artistId, $name, $formDate, $breakupDate, $formationZipcode);
+            }
+            
+            if(!$has_error) {
+                header('Location: artists.php?action=list', true);
+                die();
+            }
         }
     }
-}
-    
-// Display add artist page
-?>
-<form action="" method="POST">
-<table>
-    <tr>
-        <td>Name:</td>
-        <td><input type="text" name="name" style="width:100%" value="<?=$name?>"></input>
-        <?php if(!empty($name_error)) { ?>
-        <span class="error">* <?=$name_error?></span>
-        <?php } ?>
-        </td>
-    </tr>
-    <tr>
-        <td>Date Formed:</td>
-        <td><input type="date" name="formDate" style="width:100%" value="<?=$formDate?>"></input>
-        <?php if(!empty($formDate_error)) { ?>
-        <span class="error">* <?=$formDate_error?></span>
-        <?php } ?>
-        </td>
-    </tr>
-    <tr>
-        <td>Date Disbanded:</td>
-        <td><input type="date" name="breakupDate" style="width:100%" value="<?=$breakupDate?>"></input>
-        <?php if(!empty($breakupDate_error)) { ?>
-        <span class="error">* <?=$breakupDate_error?></span>
-        <?php } ?>
-        </td>
-    </tr>
-    <tr>
-        <td>Formation Zipcode:</td>
-        <td><input type="number" name="formationZipcode" min="10000" max="99999" style="width:100%" value="<?=$formationZipcode?>"></input>
-        <?php if(!empty($formationZipcode_error)) { ?>
-        <span class="error">* <?=$formationZipcode_error?></span>
-        <?php } ?>
-        </td>
-    </tr>
-    <tr>
-        <td colspan="2" align="center">
-            <input type="hidden" name="artistId" value="<?=$artistId?>">
-            <input type="submit" value="Submit" style="width:100%"></input>
-         </td>
-    </tr>
-</table>
-</form>
+        
+    // Display add artist page
+    ?>
+    <form action="" method="POST">
+    <table>
+        <tr>
+            <td>Name:</td>
+            <td><input type="text" name="name" style="width:100%" value="<?=$name?>"></input>
+            <?php if(!empty($name_error)) { ?>
+            <span class="error">* <?=$name_error?></span>
+            <?php } ?>
+            </td>
+        </tr>
+        <tr>
+            <td>Date Formed:</td>
+            <td><input type="date" name="formDate" style="width:100%" value="<?=$formDate?>"></input>
+            <?php if(!empty($formDate_error)) { ?>
+            <span class="error">* <?=$formDate_error?></span>
+            <?php } ?>
+            </td>
+        </tr>
+        <tr>
+            <td>Date Disbanded:</td>
+            <td><input type="date" name="breakupDate" style="width:100%" value="<?=$breakupDate?>"></input>
+            <?php if(!empty($breakupDate_error)) { ?>
+            <span class="error">* <?=$breakupDate_error?></span>
+            <?php } ?>
+            </td>
+        </tr>
+        <tr>
+            <td>Formation Zipcode:</td>
+            <td><input type="number" name="formationZipcode" min="10000" max="99999" style="width:100%" value="<?=$formationZipcode?>"></input>
+            <?php if(!empty($formationZipcode_error)) { ?>
+            <span class="error">* <?=$formationZipcode_error?></span>
+            <?php } ?>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2" align="center">
+                <input type="hidden" name="artistId" value="<?=$artistId?>">
+                <input type="submit" value="Submit" style="width:100%"></input>
+             </td>
+        </tr>
+    </table>
+    </form>
 <?php
 }
 else if($_GET['action'] == "deleteartist") {
-is_moderator_or_die();
+    is_moderator_or_die();
 
-if(!isset($_GET['id'])) {
-    die("Must specify id for this action");
+    if(!isset($_GET['id'])) {
+        die("Must specify id for this action");
+    }
+
+    $artistId = intval($_GET['id']);
+    $ret = remove_artist($artistId);
+
+    // Check error code on delete?
+    header('Location: artists.php?action=list', true);
 }
+else if($_GET['action'] == "addperformance" || $_GET['action'] == "editperformance") {
+    is_moderator_or_die();
 
-$artistId = intval($_GET['id']);
-$ret = remove_artist($artistId);
+    if(!isset($_GET['id'])) {
+        die("Must specify id for this action");
+    }
 
-// Check error code on delete?
-header('Location: artists.php?action=list', true);
+    $artistId = intval($_GET['id']);
+    $performanceId = -1;
+
+    $venueId = 0;
+    $venueId_error = "";
+
+    $duration = 0;
+    $duration_error = "";
+
+    $date = "";
+    $date_error = "";
+
+    if($_GET['action'] == "editperformance" && isset($_GET['performanceId'])) {
+        $performanceId = intval($_GET['performanceId']);    
+        $details = get_performance_details($performanceId);
+        
+        $vanueId = $details['venueId'];
+        $duration = $details['duration'];
+        $date = $details['date'];
+    }
+
+    if($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $venueId = intval($_POST['venueId']);
+        $duration = doubleval($_POST['duration']);
+        $date = sanitize_input($_POST['date']);
+        
+        $artistId = intval($_POST['artistId']);
+            
+        if(isset($_POST['performanceId'])) {
+            $performanceId = intval($_POST['performanceId']);
+        }
+        
+        $has_error = false;
+        
+        if(!$has_error) {
+            // Successful
+            
+            if($performanceId == -1) {
+                $ret = add_performance($venueId, $duration, $date);
+            } else {
+                $ret = update_performance($performanceId, $venueId, $duration, $date);
+            }
+            
+            if(!$has_error) {
+                header('Location: artists.php?action=list', true);
+                die();
+            }
+        }
+    }
+    ?>
+
+    <form action="" method="POST">
+    <table>
+        <tr>
+            <td>Venue:</td>
+            <td>
+                <select name="venueId" style="width:100%">
+                    <option value="0">Venue 1</option>
+                    <option value="1">Venue 2</option>
+                    <option value="2">Venue 3</option>
+                </select>
+            </td>
+        </tr>
+        <tr>
+            <td>Duration:</td>
+            <td><input type="number" name="duration" min="0" step="0.01" style="width:100%" value="<?=$duration?>"></input>
+            <?php if(!empty($duration_error)) { ?>
+            <span class="error">* <?=$duration_error?></span>
+            <?php } ?>
+            </td>
+        </tr>
+        <tr>
+            <td>Date:</td>
+            <td><input type="date" name="date" style="width:100%" value="<?=$date?>"></input>
+            <?php if(!empty($date_error)) { ?>
+            <span class="error">* <?=$date_error?></span>
+            <?php } ?>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2" align="center">
+                <input type="hidden" name="artistId" value="<?=$artistId?>">
+                <input type="hidden" name="performanceId" value="<?=$performanceId?>">
+                <input type="submit" value="Submit" style="width:100%"></input>
+             </td>
+        </tr>
+    </table>
+    </form>
+
+<?php
+}
+else if($_GET['action'] == "deleteperformance") {
+    is_moderator_or_die();
+
+    if(!isset($_GET['id'])) {
+        die("Must specify id for this action");
+    }
+
+    $performanceId = intval($_GET['id']);
+    $ret = remove_performance($performanceId);
+
+    // Check error code on delete?
+    header('Location: artists.php?action=list', true);
 }
 ?>
