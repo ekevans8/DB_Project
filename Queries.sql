f2ac4cf64949dd1fc07b68bc316c3ba5cb3727c7
polaris- <nagatospam@gmail.com>
2015-11-20 17:56:22 -0500

    Fix moderator flags

--- Queries.sql
+++ Queries.sql
@@ -1,9 +1,9 @@
 #Insert Test Users
-INSERT INTO user (username, firstName, lastName, age, email, password, zipcode, isModerator) VALUES ('B-Dawg', 'Brenton', 'Smelser', '25', 'bsmels1@students.towson.edu', '$2y$10$xtYuAsiiwn2TQCMUvREJT.y7eXC/h6b7ZDG1vlOhNhJiLJgjGv6VW', '21040', '1');
-INSERT INTO user (username, firstName, lastName, age, email, password, zipcode, isModerator) VALUES ('E-Dawg', 'Emily', 'Evans', '21', 'eevans7@studnets.towson.edu', '$2y$10$bp/WdkFArWpmfgCuvmzRje.cONMYbH2Jc1p4/dgrZj9FR2wy9Yz8G', '21252', '1');
-INSERT INTO user (username, firstName, lastName, age, email, password, zipcode, isModerator) VALUES ('L-Dawg', 'Larry', 'McLovin', '19', 'McLovin@gmail.com', '$2y$10$bZJ4rakwZWdTJdjDWIawo..11PByJIQwr1C.3FJRnm.GLihVzgSaG', '21207', '2');
-INSERT INTO user (username, firstName, lastName, age, email, password, zipcode, isModerator) VALUES ('M-Dawg', 'Miguel', 'Fernandez', '21', 'mferna6@students.towson.edu', '$2y$10$/8XGWtCMeXgYyl.Cvpw7P.89jix7OleKdaRPBXC0qJebso3iTAjFG', '21207', '1');
-INSERT INTO user (username, firstName, lastName, age, email, password, zipcode, isModerator) VALUES ('S-Dawg', 'Shannon', 'Smith', '21', 'smith3@students.towson.edu', '$2y$10$xyZOdBaa/Gg/Cql0zDgSdOQde4P7jBFpBCcOS.gylV4j6BIjWZK7S', '21252', '1');
+INSERT INTO user (username, firstName, lastName, age, email, password, zipcode, isModerator) VALUES ('B-Dawg', 'Brenton', 'Smelser', '25', 'bsmels1@students.towson.edu', '$2y$10$xtYuAsiiwn2TQCMUvREJT.y7eXC/h6b7ZDG1vlOhNhJiLJgjGv6VW', '21040', '0');
+INSERT INTO user (username, firstName, lastName, age, email, password, zipcode, isModerator) VALUES ('E-Dawg', 'Emily', 'Evans', '21', 'eevans7@studnets.towson.edu', '$2y$10$bp/WdkFArWpmfgCuvmzRje.cONMYbH2Jc1p4/dgrZj9FR2wy9Yz8G', '21252', '0');
+INSERT INTO user (username, firstName, lastName, age, email, password, zipcode, isModerator) VALUES ('L-Dawg', 'Larry', 'McLovin', '19', 'McLovin@gmail.com', '$2y$10$bZJ4rakwZWdTJdjDWIawo..11PByJIQwr1C.3FJRnm.GLihVzgSaG', '21207', '1');
+INSERT INTO user (username, firstName, lastName, age, email, password, zipcode, isModerator) VALUES ('M-Dawg', 'Miguel', 'Fernandez', '21', 'mferna6@students.towson.edu', '$2y$10$/8XGWtCMeXgYyl.Cvpw7P.89jix7OleKdaRPBXC0qJebso3iTAjFG', '21207', '0');
+INSERT INTO user (username, firstName, lastName, age, email, password, zipcode, isModerator) VALUES ('S-Dawg', 'Shannon', 'Smith', '21', 'smith3@students.towson.edu', '$2y$10$xyZOdBaa/Gg/Cql0zDgSdOQde4P7jBFpBCcOS.gylV4j6BIjWZK7S', '21252', '0');
 
 #Insert Test Venues
 INSERT INTO venue (name, streetAddress, city, state, zipcode) VALUES ('Pier Six Box Office', '731 Eastern Ave', 'Baltimore', 'MD', '21202');
