50f79b166ef855415d56bc92a10a8d1f996d5f61
polaris- <nagatospam@gmail.com>
2015-11-20 15:12:53 -0500

    Get user favorites

--- profile.php
+++ profile.php
@@ -49,10 +49,12 @@ if(!is_moderator($user_profile->username)) {
 <a href="artists.php?action=list">View Artists</a><br>
 <br>
 <b><?=$user_profile->username?>'s Favorite artists</b><br>
-Band A...<br>
-Band B...<br>
-Band C...<br>
-Band D...<br>
+<?php
+$favorites = get_all_usernames_and_favorites_per_favorite($username);
+foreach($favorites as $favorite) {
+    echo '<a href="artists.php?action=details&id=' . $favorite['artistId'] . '">' . $favorite['artistName'] . '</a><br>';
+}
+?>
 <br>
 <b>Concerts <?=$user_profile->username?> has attended</b><br>
 Concert A...<br>
