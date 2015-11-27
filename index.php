3168fd023d8c3f1168923c896e7275e47427d008
ekevans8 <ekevans8@users.noreply.github.com>
2015-11-27 12:32:00 -0500

    Add "About the project"

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
 include("header.php");
 ?>
-<p>About the project</p>
+<p>The Music Tracker is a way for users to store information about their favorite artists and songs, as well as the performaces they''ve seen. Users can add comments on artists'' performances, and can view the profiles of other users who have similar tastes. The Music Tracker incorporates the use of a database in order to store all of the users'' information in one place. </p>
