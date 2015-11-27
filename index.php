db833460c0e9745877feda0182d6c254e539b888
ekevans8 <ekevans8@users.noreply.github.com>
2015-11-27 15:20:00 -0500

    Update index.php
    Weird apostrophes

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 include("header.php");
 ?>
-<p>The Music Tracker is a way for users to store information about their favorite artists and songs, as well as the performaces they''ve seen. Users can add comments on artists'' performances, and can view the profiles of other users who have similar tastes. The Music Tracker incorporates the use of a database in order to store all of the users'' information in one place. 
+<p>The Music Tracker is a way for users to store information about their favorite artists and songs, as well as the performaces they've seen. Users can add comments on artists' performances, and can view the profiles of other users who have similar tastes. The Music Tracker incorporates the use of a database in order to store all of the users' information in one place. 
 <br><br>
 By: 
 <br>
